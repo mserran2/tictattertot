7bd7346552b767fb708f329f67ac143717f9f93c
Mark Serran <mserran2@swarthmore.edu>
2014-03-13 19:12:17 -0400

    - Initial code commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictattertot::Application.config.secret_key_base = '584ab62efca31afe47201b13f3fe14282d505c5287d18107ea9ce058adad8da5303ed3c1bdd8787ffcc0ad64da67a113b9cf269db0f22d4e3145fad8e474b907'
